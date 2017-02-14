b822838f12a921cb3205ba2000b9e782fa7da1df
duy <buiduylinhtb@gmail.com>
2017-02-14 12:58:21 +0700

    them anh

--- app/src/main/java/hust/linh/randomchat/MainActivity.java
+++ app/src/main/java/hust/linh/randomchat/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ///////dâdadadada
+        System.out.println("xin chao");
     }
 }
